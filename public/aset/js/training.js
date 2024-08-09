a03b56b0fb328ea7b449d0d94cdfaa0a1b244e02
fakhryhizballah <falehry88@gmail.com>
2024-08-09 10:06:53 +0700

    remove test

--- public/aset/js/training.js
+++ public/aset/js/training.js
@@ -7,14 +7,14 @@ function train(id) {
             tableSiswa.ajax.reload();
         }
     })
-    $.ajax({
-        url: '/api/test',
-        method: 'GET',
-        success: function (data) {
-            console.log(data);
-            $('#cardText').text(`Accuracy decision tree ' + ${data.data.DecisionTree} + ' : ` + data.accuracy.toFixed(2) + '%`);
-            // tableLabel.ajax.reload();
-        }
-    })
+    // $.ajax({
+    //     url: '/api/test',
+    //     method: 'GET',
+    //     success: function (data) {
+    //         console.log(data);
+    //         $('#cardText').text(`Accuracy decision tree ' + ${data.data.DecisionTree} + ' : ` + data.accuracy.toFixed(2) + '%`);
+    //         // tableLabel.ajax.reload();
+    //     }
+    // })
 }
 
