5af08999380f6ee992314f5a96ffb0dc63475a6c
fakhryhizballah <falehry88@gmail.com>
2024-09-10 20:44:15 +0700

    tree

--- app/Views/predict.php
+++ app/Views/predict.php
@@ -21,15 +21,42 @@
             </table>
         </div>
         <div class="card-footer bg-transparent text-end"><button id="Train" name="Train" onclick="train('dtree')" class="btn btn-primary">Train/Test</button></div>
-
     </div>
+    <div class="card mt-2">
+        <h5 class="card-title text-center">Native Bayes</h5>
+        <div class="card-body">
+            <div id="wrapper">
+                <span class="label">
+                    Apakah ada nilai Mental Kepribadian Siswa ≥ 4,5
+                </span>
+                <div class="branch lv1">
+                    <div class="entry ">
+                        <span class="label">TIDAK BERPRESTASI</span>
+                    </div>
+                    <div class="entry ">
+                        <span class="label">Apakah terdapat minimal 2 nilai yang ≥ 4,5</span>
+                        <div class="branch lv1">
+                            <div class="entry ">
+                                <span class="label">TIDAK BERPRESTASI</span>
+                            </div>
+                            <div class="entry ">
+                                <span class="label">BERPRESTASI</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 
 <?= $this->endSection('layout'); ?>
 
 <?= $this->section('css'); ?>
-<link rel="stylesheet" href="https://cdn.datatables.net/2.1.0/css/dataTables.dataTables.min.css" />
+<link rel=" stylesheet" href="https://cdn.datatables.net/2.1.0/css/dataTables.dataTables.min.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+<link rel="stylesheet" href="/aset/css/tree.css" />
 <?= $this->endSection('css'); ?>
 
 <?= $this->section('script'); ?>
